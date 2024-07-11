ae6b974e8311a284663fab0cf7c8e21a9d5c2119
hmontes <hugomontes927@gmail.com>
2024-07-11 09:32:32 -0400

    22. Middleware de autenticación (JWT)

---
+++ middlewares/authenticated.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Importar los modulos necesarios
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'clave_secreta';
+
+// Crear una funcion para comprobar si los datos del token son correctos
+exports.ensureAuth = function (req, res, next) {
+    // Verificar si existe la cabecera de authorization
+    if (!req.headers.authorization) {
+        return res.status(403).send({ message: 'La peticion no tiene la cabecera de autenticacion' });
+    }
+    // Obtener el token de authorization 
+    // Quitar las comillas simples y dobles que podrian estar al inicio y al final de la cadena
+    // Reemplazar las comillas dobles y simples por una cadena vacia ''
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+
+    // Captura el error al momento de codificar el token
+    try {
+        // Obtener el objeto que esta guardado en el token
+        var payload = jwt.decode(token, secret);
+        // Verificar si la fecha de expiracion es menor a la fecha actual
+        if (payload.exp <= moment().unix()) {
+            return res.status(401).send({ message: 'El token ha expirado' });
+        }
+    } catch (ex) {
+        console.log(ex);
+        return res.status(404).send({ message: 'Token no valido' });
+    }
+    // Adicionar al request los datos del usuario/payload para evitar decodificar dentro un
+    // controlador que necesite los datos del usuario.
+    // Dentro de un controlador obtendremos los datos de user desde req
+    req.user = payload;
+    // Salimos del middleware con next()
+    next();
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,12 @@ var express = require('express');
 var UserController = require('../controllers/userController');
 // Cargar ruter de express
 var api = express.Router();
+// Importar el middleware para autenticacion
+var md_auth = require('../middlewares/authenticated');
 
 // Crear la ruta y asociarlo con el metodo del controlador
-api.get('/probando-controlador', UserController.pruebas);
+// Para usar un middleware lo pasamos como segundo parametro en el metodo.
+api.get('/probando-controlador', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
