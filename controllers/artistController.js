d95d05da8fdbc52fb4424a4384dfc7bd814b4d1b
hmontes <hugomontes927@gmail.com>
2024-07-15 16:04:20 -0400

    27. Crear un artista

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -13,6 +13,30 @@ function getArtist(req, res) {
     res.status(200).send({ message: 'Metodo getArtist del controlador artist.js' });
 }
 
+// Metodo para guardar un artista
+async function saveArtist(req, res) {
+    try {
+        var artist = new Artist();
+        // Obtener los datos a guardar
+        var params = req.body;
+        artist.name = params.name;
+        artist.description = params.description;
+        artist.image = null;
+        // Guardar datos
+        const artistStored = await artist.save();
+        if (!artistStored) {
+            return res.status(400).send({ message: 'No se ha registrado el artista' });
+        }
+        return res.status(200).send({
+            message: 'Datos guardados correctamente.',
+            artist: artistStored
+        });
+    } catch (error) {
+        return res.status(500).send({ message: 'Error al registrar artista' });
+    }
+}
+
 module.exports = {
-    getArtist
+    getArtist,
+    saveArtist
 }
--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -8,6 +8,7 @@ var md_auth = require('../middlewares/authenticated');
 
 // Crear la ruta y asociarlo con el metodo del controlador
 api.get('/artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 
 // Exportar metodos del API
 module.exports = api;
